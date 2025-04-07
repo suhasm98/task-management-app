3ea5bf1f800d52805f92d2c0b9d2eaffd385b8f7
suhasm98 <suhasmruthyunjaya98@gmail.com>
2025-04-07 17:57:02 +0530

    add server-side middlewares

---
+++ server-side/middleware/requireRole.js
@@ -0,0 +1,9 @@
+module.exports = function requireRole(role) {
+    return (req, res, next) => {
+      if (!req.user || req.user.role !== role) {
+        return res.status(403).json({ message: "Forbidden: Insufficient role" });
+      }
+      next();
+    };
+  };
+  
