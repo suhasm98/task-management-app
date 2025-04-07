d0b9cc60765a011c62c764ee27468c3ad5d23f99
suhasm98 <suhasmruthyunjaya98@gmail.com>
2025-04-07 18:55:39 +0530

    update cors usage

--- server-side/server.js
+++ server-side/server.js
@@ -24,13 +24,7 @@ const limiter = rateLimit({
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("CORS not allowed for this origin"));
-      }
-    },
+    origin: allowedOrigins,
     credentials: true,
   })
 );
